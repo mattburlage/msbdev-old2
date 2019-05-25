b853c1d8cece34a5754bc5048157d5ad7f75b736
Matthew Smith-Burlage <mattburlage@gmail.com>
2019-05-25 18:15:54 -0400

    initial commit

--- msbdevsite/urls.py
+++ msbdevsite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('pieandcheese/', admin.site.urls),
     path('', include(('msbdev.urls', 'msbdev'), namespace='msbdev')),
 ]

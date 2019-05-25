55348c0e4bef0efcce4a4b280fbbc53f8b6bdbdf
Matt Burlage <mattburlage@gmail.com>
2019-05-25 08:35:11 +0000

    deployed

--- msbdevsite/settings.py
+++ msbdevsite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '192.168.86.109',
-    'msbdevwebserver'
+    'msbdevwebserver',
     'msb.dev',
     'www.msb.dev',
 ]
@@ -125,9 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = '/msbdevsite/site/public/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    # '/var/www/static/',
-]
+#STATICFILES_DIRS = [
+#	os.path.join(BASE_DIR, "static"),
+#]
